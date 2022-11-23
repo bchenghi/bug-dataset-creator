bcb036bab0c13ecf8e571db7b0240e6ea5c36014
david <ykwongaq@connect.ust.hk>
2022-11-23 22:24:04 +0800

    Setup for label assignment

--- src/main/java/label/Main.java
+++ src/main/java/label/Main.java
@@ -1,7 +1,27 @@
 package label;
 
+import microbat.model.trace.Trace;
+import tregression.model.PairList;
+import tregression.separatesnapshots.DiffMatcher;
+import tregression.tracematch.ControlPathBasedTraceMatcher;
+
 /*
 Temporary file
  */
 public class Main {
+	
+	public void assignLabel(Trace correctTrace, Trace buggyTrace) {
+		
+		// Below are the setup 
+		final String srcFolderPath = "";
+		final String testFolderPath = "";
+		final String mutatedProjPath = "";
+		final String originalProjectPath = "";
+		
+		final DiffMatcher matcher = new DiffMatcher(srcFolderPath, testFolderPath, mutatedProjPath, originalProjectPath);
+		matcher.matchCode();
+		
+		ControlPathBasedTraceMatcher traceMatcher = new ControlPathBasedTraceMatcher();
+		final PairList pairList = traceMatcher.matchTraceNodePair(buggyTrace, correctTrace, matcher);
+	}
 }
