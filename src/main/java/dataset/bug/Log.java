208475c93830aa9092ee44348d8cae7be2087e1e
bchenghi <bohchenghin@gmail.com>
2022-12-03 21:12:57 +0800

    Add size limit and file rotation to logger

--- src/main/java/dataset/bug/Log.java
+++ src/main/java/dataset/bug/Log.java
@@ -7,11 +7,13 @@ import java.util.logging.Logger;
 public class Log {
 	public static final String DEFAULT_LOG_FILE_PATTERN = "%t/dataset-creator%g.log";
 	private static FileHandler fileHandler = null;
+	private static int LOG_FILE_SIZE = 1 * (int) Math.pow(10, 9); // 1GB
+	private static int NUM_LOG_FILES = 4;
 	public static Logger createLogger(Class<?> someClass) {
 		Logger logger = Logger.getLogger(someClass.getName());
 		try {
 			if (fileHandler == null) {
-				fileHandler = new FileHandler(DEFAULT_LOG_FILE_PATTERN, true);
+				fileHandler = new FileHandler(DEFAULT_LOG_FILE_PATTERN, LOG_FILE_SIZE, NUM_LOG_FILES);
 			}
 			logger.addHandler(fileHandler);
 		} catch (IOException e) {
